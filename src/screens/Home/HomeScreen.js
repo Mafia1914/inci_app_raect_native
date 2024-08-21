2d26e22b51d1cbc8fe5acac17c4be68c156ca805
Mafia1914 <mafia.zameer@bitverx.io>
2024-08-21 13:04:30 +0500

    add app logo android and ios

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import BottomContainer from './components/bottomConatiner'
+
 
 export default function HomScreen() {
   return (
